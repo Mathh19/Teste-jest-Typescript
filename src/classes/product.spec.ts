6af4947b1fb2844b2ef495c1197461689b322956
Matheus <mf635097@gmail.com>
2022-04-11 14:26:18 -0300

    Testando Discount.ts

--- src/classes/product.spec.ts
+++ src/classes/product.spec.ts
@@ -7,7 +7,7 @@ const createSut = (name: string, price: number): Product => {
 describe('Product', () => {
   afterEach(() => jest.clearAllMocks());
 
-  it('should return undefined', () => {
+  it('should have property name and price', () => {
     const sut = createSut('Camiseta', 59.9);
     expect(sut).toHaveProperty('name', 'Camiseta');
     expect(sut.price).toBeCloseTo(59.9);
